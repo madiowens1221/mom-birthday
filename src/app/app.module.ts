a765dad3e734034c8924a5e2cb7206aa80a17347
Madison Owens <madison.owens1221@gmail.com>
2018-11-14 23:38:35 -0600

    deploying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatIconModule,    
+    MatTooltipModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
